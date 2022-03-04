95d0de9b4320032d1b46ba64ac868c2c4ea9e729
LeoM <leomccarthy709@gmail.com>
2022-03-04 12:41:41 +0000

    function to clean finishing position

--- arion/datasets/transform.py
+++ arion/datasets/transform.py
@@ -1,26 +1,33 @@
-from unittest import result
 import pandas as pd
+import numpy as np
+
 
 class RaceDataTransformer:
     def __init__(self) -> None:
         pass
-    
+
+
     def clean(self, df: pd.DataFrame) -> pd.DataFrame:
         df['datetime'] = pd.to_datetime(df['datetime'], utc=True)
-        df.sort_values(by = 'datetime', inplace = True)
+        df = df.sort_values(by = 'datetime')
 
         df['rating_band'] = df['rating_band'].fillna('none_handicap')
 
         df = self.fill_runners(df)
 
+        none_finish = [not i.strip().isnumeric() for i in df['finish_pos']]
+        df['finish_pos'] =  np.where(none_finish, df['runners'], df['finish_pos']).astype('int8')      
+
         return df
 
     @staticmethod
-    def fill_runners(df: pd.DataFrame):
+    def fill_runners(df: pd.DataFrame) -> pd.DataFrame:
         runners = df.groupby('race_id')['horse_name'].transform('count')
 
         df['runners'] = df['runners'].fillna(runners)
 
         return df
+
+        
 
 
