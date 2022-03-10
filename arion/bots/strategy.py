3dcd5f26fd4fbe6cee73f6e0defbd0a491eb9549
LeoM <leomccarthy709@gmail.com>
2022-03-10 16:42:15 +0000

    bot strategy, get selection names and prices to pass to model

--- arion/bots/strategy.py
+++ arion/bots/strategy.py
@@ -1,29 +1,59 @@
-import flumine
+import os
+from datetime import datetime, timedelta
+
 import betfairlightweight
+import flumine
+import pandas as pd
 from betfairlightweight import StreamListener
 from dotenv import load_dotenv
-from flumine import Flumine, clients, BaseStrategy
-from flumine.order.trade import Trade
+from flumine import BaseStrategy, Flumine, clients
 from flumine.order.order import OrderStatus
 from flumine.order.ordertype import LimitOrder
+from flumine.order.trade import Trade
 from flumine.utils import get_price
-import os
-
 from matplotlib.cbook import print_cycles
 
 
 class BetPlacer(BaseStrategy):
     def check_market_book(self, market, market_book):
-        if market_book.status != "CLOSED":
+        time_to_off = market_book.market_definition.market_time - datetime.now()
+        if market_book.status != "CLOSED" and time_to_off < timedelta(hours=8):
             return True
 
-
     def process_market_book(self, market, market_book):
-        prices = {
-            r.selection_id:r.ex.available_to_back[0]['price']
-            for r in market_book.runners
-            if r.status == "ACTIVE" 
-        }
-        print(prices)
-        print([runner.runner_name for runner in market.market_catalogue.runners])
-    
+        prices = pd.DataFrame(
+            [
+                {
+                    "selection_id": r.selection_id,
+                    "price": r.ex.available_to_back[0]["price"],
+                }
+                for r in market_book.runners
+                if r.status == "ACTIVE"
+            ]
+        )
+        names = pd.DataFrame(
+            [
+                {"selection_id": runner.selection_id, "horse_name": runner.runner_name}
+                for runner in market.market_catalogue.runners
+            ]
+        )
+
+
+def clean_strings(string: str, type: str = "str") -> str:
+
+    return (
+        string.strip()
+        .replace(",", " ")
+        .replace("'", "")
+        .replace('"', "")
+        .replace(")", "")
+        .replace("(", "")
+        .title()
+        .replace(".", "")
+        .replace("\x80", "")
+        .replace("\\x80", "")
+        .replace("\xa0", " ")
+        .replace("\n", " ")
+        .replace("      ", "")
+        .replace("\r", "")
+    )
--- main.py
+++ main.py
@@ -1,24 +1,28 @@
-import os 
-from dotenv import load_dotenv
+import os
+
 import betfairlightweight
-from arion.bots.strategy import BetPlacer
-from betfairlightweight.filters import streaming_market_filter, streaming_market_data_filter
-from flumine import Flumine, clients
+from betfairlightweight.filters import (streaming_market_data_filter,
+                                        streaming_market_filter)
 from betfairlightweight.resources import MarketCatalogue
+from dotenv import load_dotenv
+from flumine import Flumine, clients
 from flumine.markets.middleware import Middleware
+
+from arion.bots.strategy import BetPlacer
 
 load_dotenv()
 
 trading = betfairlightweight.APIClient(
-            os.getenv("BFAIR_USERNAME"),
-            os.getenv("BFAIR_PASSWORD"),
-            app_key=os.getenv("BFAIR_KEY"),
-            certs="arion/certs",
-        )
+    os.getenv("BFAIR_USERNAME"),
+    os.getenv("BFAIR_PASSWORD"),
+    app_key=os.getenv("BFAIR_KEY"),
+    certs="arion/certs",
+)
 trading.betting.read_timeout = 30
 client = clients.BetfairClient(trading, paper_trade=True)
 framework = Flumine(client=client)
-strategy = BetPlacer( market_filter=streaming_market_filter(
+strategy = BetPlacer(
+    market_filter=streaming_market_filter(
         event_type_ids=["7"],
         country_codes=["GB"],
         market_types=["WIN"],
@@ -27,4 +31,3 @@ strategy = BetPlacer( market_filter=streaming_market_filter(
 framework.add_strategy(strategy)
 
 framework.run()
-
